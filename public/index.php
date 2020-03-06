f549bfdc0092a1f1a2a2a382dff6412d46453819
Alex Dingal <lhexie2014@gmail.com>
2020-03-07 02:06:38 +0800

    Move all footer items to private shared_items folder

--- public/index.php
+++ public/index.php
@@ -1,19 +1,7 @@
 <?php include('../private/shared_items/main_header.php'); ?>
+
     <div id="content">
         
     </div>
-    <footer id="main-footer" class="bg-dark text-light fixed-bottom py-3">
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <p class="text-right m-0">Copyright &copy; <?php echo Date('Y'); ?></p>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="js/index.js"></script>
-</body>
-</html>
+    
+<?php include('../private/shared_items/main_footer.php'); ?>
