affb2a89b901c075f449e055338547fd29e7b3bd
Alex Dingal <lhexie2014@gmail.com>
2020-03-07 12:14:04 +0800

    Create login form

--- public/index.php
+++ public/index.php
@@ -1,7 +1,26 @@
 <?php include('../private/shared_items/main_header.php'); ?>
 
     <div id="content">
-        
+       <div id="login" class="py-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-lg-4 mx-auto">
+                        <form action="#">
+                            <h1 class="text-center">Sign in...</h1>
+                            <div class="form-group">
+                                <input type="email" name="email" placeholder="Email" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" name="password" placeholder="Password" class="form-control">
+                            </div>
+                            <button class="btn btn-secondary btn-block">
+                                Login
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+       </div> 
     </div>
     
 <?php include('../private/shared_items/main_footer.php'); ?>
