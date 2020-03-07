d5ce3c7afad3d10e85b9cedef49996fce4d6898a
Alex Dingal <lhexie2014@gmail.com>
2020-03-07 23:57:40 +0800

    Add dummy function to simulate user authentication

--- private/authentication.php
+++ private/authentication.php
@@ -1,6 +1,8 @@
-function is_logged_in() {
-    // Having a admin_id in the session serves a dual-purpose:
-    // - Its presence indicates the admin is logged in.
-    // - Its value tells which admin for looking up their record.
-    return isset($_SESSION['admin_id']);
-}
+<?php
+
+
+    function is_logged_in() {
+        return true;
+    }
+
+?>
