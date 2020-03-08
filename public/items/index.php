f72124cb7d4395b4f1c90200af72a88649b40621
Alex Dingal <lhexie2014@gmail.com>
2020-03-09 00:18:10 +0800

    Create initial code for items index.php and update javascript file path

---
+++ public/items/index.php
@@ -0,0 +1,13 @@
+<?php
+    require_once('../../private/init.php');
+
+    $page_title = 'Items';
+?>
+
+<?php include(SHARED_PATH . '/main_header.php'); ?>
+
+<div id="content">
+
+</div>
+
+<?php include(SHARED_PATH . '/main_footer.php'); ?>
