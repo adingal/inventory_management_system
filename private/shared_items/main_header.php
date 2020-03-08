341e15e3664a81e2579ada61bda596b7ebe9f53d
Alex Dingal <lhexie2014@gmail.com>
2020-03-09 00:51:18 +0800

    Update css link for main header using constants

--- private/shared_items/main_header.php
+++ private/shared_items/main_header.php
@@ -10,7 +10,7 @@
     <title><?php echo $page_title; ?></title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="<?php echo WWW_ROOT . '/css/index.css'; ?>">
 </head>
 <body>
     <nav id="main-header" class="navbar navbar-expand-md navbar-dark bg-dark p-md-0">
