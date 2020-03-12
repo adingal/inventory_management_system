73de5ffc6feffbdc4600c1f1559cb9d4853c1f25
Alex Dingal <lhexie2014@gmail.com>
2020-03-13 00:04:03 +0800

    Create dummy user

---
+++ private/db_credentials.php
@@ -0,0 +1,10 @@
+<?php
+    // User Details
+    // CREATE USER 'imsuser'@'localhost'
+    // IDENTIFIED BY '123IMS';
+
+    // GRANT SELECT, INSERT, UPDATE, DELETE ON inventory_management_system.*
+    // TO 'imsuser'@'localhost';
+
+
+?>
