44447a906d05e2a2d7a2db42a4f86b4eff05a77c
Alex Dingal <lhexie2014@gmail.com>
2020-03-12 23:55:06 +0800

    Create database schema and insert dummy data

---
+++ public/items/items.sql
@@ -0,0 +1,43 @@
+CREATE TABLE items (
+    item_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    item_name VARCHAR(255) NOT NULL,
+    item_description TEXT NOT NULL,
+    quantity INT UNSIGNED NOT NULL,
+    added_by VARCHAR(255) NOT NULL,
+    added_date DATETIME NOT NULL,
+    PRIMARY KEY (item_id),
+    INDEX (added_date)
+) ENGINE = INNODB;
+
+
+-- Dummy data
+INSERT INTO items (item_name, item_description, quantity, added_by, added_date)
+VALUES
+    (
+    'R52M-1A',
+    'Resistor with 52 megaohms resistance and 1 ampere current capacity.',
+    '10',
+    'Juan Dela Cruz',
+    NOW()
+    ),
+    (
+    'R68O-5A',
+    'Resistor with 68 ohms resistance and 5 amperes current capacity.',
+    '23',
+    'Jose Manalo',
+    NOW() - 2
+    ),
+    (
+    'R1M-3A',
+    'Resistor with 1 megaohm resistance and 3 amperes current capacity.',
+    '2',
+    'Rufino David',
+    NOW() + 1
+    ),
+    (
+    'R50O-2A',
+    'Resistor with 50 ohms resistance and 2 amperes current capacity.',
+    '54',
+    'Freddy Aguilar',
+    NOW() + 4
+    );
