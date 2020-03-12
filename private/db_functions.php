fd9d5b588bb745d531bdfc3ad2b7466ae8cd85e3
Alex Dingal <lhexie2014@gmail.com>
2020-03-13 00:17:23 +0800

    Create helper functions for database connection

---
+++ private/db_functions.php
@@ -0,0 +1,40 @@
+<?php
+
+    // Connect to database
+    function db_connect() {
+        $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+        confirm_connection();
+        return $connection;
+    }
+
+    // Disconnect to database
+    function db_disconnect($connection) {
+        if (isset($connection)) {
+            mysqli_close($connection);
+        }
+    }
+
+    // Escape string before using on queries
+    function db_escape($connection, $string) {
+        return mysqli_real_escape_string($connection, $string);
+    }
+
+    // Confirm connection if establish
+    function confirm_connection() {
+        if (mysqli_connect_errno()) {
+            $msg = "Database connection failed: ";
+            $msg .= mysqli_connect_error();
+            $msg .= " (" . mysqli_connect_errno() . ")";
+
+            exit($msg);
+        }
+    }
+
+    // Confirm result set on queries
+    function confirm_result_set($result_set) {
+        if (!$result_set) {
+            exit("Database query failed.");
+        }
+    }
+
+?>
