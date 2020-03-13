48ceb2fef9710ab5a9d64b06e353b5f4bc55dedf
Alex Dingal <lhexie2014@gmail.com>
2020-03-13 22:24:42 +0800

    Create two addition function to handle redirects and root url

--- private/helper_functions.php
+++ private/helper_functions.php
@@ -1,7 +1,20 @@
 <?php
 
+    function url_for($path) {
+        // Add the leading '/' if not present
+        if ($path[0] != '/') {
+            $path = "/" . $path;
+        }
+        return WWW_ROOT . $path;
+    }
+
     function is_post() {
         return $_SERVER['REQUEST_METHOD'] == 'POST';
+    }
+
+    function redirect_to($location) {
+        header("Location: " . $location);
+        exit;        
     }
 
 
