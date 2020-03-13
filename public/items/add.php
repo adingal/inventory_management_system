4e8b3b5ed113af71e9ca2e0b4d6a3d3deaee91a0
Alex Dingal <lhexie2014@gmail.com>
2020-03-13 22:15:52 +0800

    Remove excess white space on items add page

--- public/items/add.php
+++ public/items/add.php
@@ -7,39 +7,37 @@
 <?php include(SHARED_PATH . '/main_header.php'); ?>
 
 <div id="content">
-
-<div id="add_item" class="py-5">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 mx-auto">
-                <form action="<?php echo WWW_ROOT . '/items/add.php'; ?>" method="post">
-                    <h1 class="display-5">Add new item...</h1>
-                    <div class="form-group mb-2">
-                        <label for="item_name">Item Name</label>
-                        <input type="text" class="form-control" name="item_name">
-                    </div>
-                    <div class="form-group mb-2">
-                        <label for="description">Description</label>
-                        <textarea class="form-control" name="description" cols="30" rows="5"></textarea>
-                    </div>
-                    <div class="form-group mb-2">
-                        <label for="quantity">Quantity</label>
-                        <input type="number" class="form-control" name="quantity" min="1" max="100">
-                    </div>
-                    <div class="form-group mb-2">
-                        <label for="added_by">Added By</label>
-                        <input type="text" class="form-control" name="added_by">
-                    </div>
-                    <div class="form-group mb-2 text-right">
-                        <input type="submit" class="btn btn-dark" value="Add">
-                        <a href="<?php echo WWW_ROOT . '/items/index.php'; ?>" class="btn btn-dark">Cancel</a>
-                    </div>
-                </form>
+    <div id="add_item" class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 mx-auto">
+                    <form action="<?php echo WWW_ROOT . '/items/add.php'; ?>" method="post">
+                        <h1 class="display-5">Add new item...</h1>
+                        <div class="form-group mb-2">
+                            <label for="item_name">Item Name</label>
+                            <input type="text" class="form-control" name="item_name">
+                        </div>
+                        <div class="form-group mb-2">
+                            <label for="description">Description</label>
+                            <textarea class="form-control" name="description" cols="30" rows="5"></textarea>
+                        </div>
+                        <div class="form-group mb-2">
+                            <label for="quantity">Quantity</label>
+                            <input type="number" class="form-control" name="quantity" min="1" max="100">
+                        </div>
+                        <div class="form-group mb-2">
+                            <label for="added_by">Added By</label>
+                            <input type="text" class="form-control" name="added_by">
+                        </div>
+                        <div class="form-group mb-2 text-right">
+                            <input type="submit" class="btn btn-dark" value="Add">
+                            <a href="<?php echo WWW_ROOT . '/items/index.php'; ?>" class="btn btn-dark">Cancel</a>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
-
 </div>
 
 <?php include(SHARED_PATH . '/main_footer.php'); ?>
