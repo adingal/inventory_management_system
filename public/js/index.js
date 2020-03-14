c5bafd6ccdc1730b8e33df40fefbc1f96db03e8b
Alex Dingal <lhexie2014@gmail.com>
2020-03-14 13:37:51 +0800

    Create IIFE to handle items page edit link

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-// Align form on middle and slightly higher
+// Set content height depending on current viewport
 (function setContentHeight() {
     const content = document.getElementById('content');
     const loginForm = document.querySelector('#login form');
@@ -21,4 +21,32 @@
         loginForm.setAttribute('style', 'margin-top: ' + loginMarginTop + 'px');
     }
 
+})();
+
+// Set href for edit button on items index page depending on selected id
+(function setItemsEditBtn() {
+    const selectedItem = document.getElementById('selected_item');
+    const actionsEditBtn = document.querySelector('#actions a[href*="edit.php"]');
+    
+    if (actionsEditBtn) {
+        selectedItem.addEventListener('click', handleChange);
+    
+        function handleChange() {
+            handleHref();
+            // Get currently selected id
+            const selectedID = selectedItem.options[selectedItem.selectedIndex].value;
+            // Set new href on edit button
+            const newHref = actionsEditBtn.href + '?id=' + selectedID;
+            actionsEditBtn.setAttribute('href', newHref);
+        }
+    
+        function handleHref() {
+            const curHref = actionsEditBtn.href;
+            // Find .php on href
+            const findPhp = curHref.indexOf('.php');
+            // Set new href on edit button
+            const newHref = curHref.substr(0, findPhp + 4);
+            actionsEditBtn.setAttribute('href', newHref);
+        }
+    }
 })();
