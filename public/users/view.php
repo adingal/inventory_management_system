845c4bbe071855f0d7856d15a0375b3ef27307fd
Alex Dingal <lhexie2014@gmail.com>
2020-03-17 01:49:52 +0800

    Create view user page

---
+++ public/users/view.php
@@ -0,0 +1,23 @@
+<?php
+    require_once('../../private/init.php');
+
+    $page_title = 'View User';
+
+    $id = $_GET['id'] ?? NULL;
+
+    if (is_null($id)) {
+        redirect_to(url_for('/users/index.php'));
+    }
+
+    $user = find_user_by_id($id);
+
+    
+?>
+
+<?php include(SHARED_PATH . '/main_header.php'); ?>
+
+<div id="content">
+
+</div>
+
+<?php include(SHARED_PATH . '/main_footer.php'); ?>
