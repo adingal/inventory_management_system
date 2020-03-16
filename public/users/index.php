e1ad8f23af6cb7ff747f94bbbc384a405a697da1
Alex Dingal <lhexie2014@gmail.com>
2020-03-17 00:05:47 +0800

    Loop through id and return back previous grid column from users index page

--- public/users/index.php
+++ public/users/index.php
@@ -13,19 +13,22 @@
     <div id="actions" class="pt-5 pb-2 mt-3">
         <div class="container">
             <div class="row">
-                <div id="id_options" class="col-md-3 mb-2">
+                <div id="id_options" class="col-md-4 mb-2">
                     <div class="input-group">
                         <div class="input-group-prepend">
                             <label class="input-group-text" for="selected_item">User ID</label>
                         </div>
                         <select class="custom-select" id="selected_item">
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
+                            <?php
+                                while ($user_id = mysqli_fetch_row($user_ids)) {
+                                    echo '<option value="' . h($user_id[0]) . '">';
+                                    echo h($user_id[0]) . '</option>';
+                                }
+                            ?>      
                         </select>
                     </div>
                 </div>
-                <div class="col-md-9 text-center text-md-right">
+                <div class="col-md-8 text-center text-md-right">
                     <a href="#" class="btn btn-primary"><i class="fa fa-plus"></i> Add</a>
                     <a href="<?php echo url_for('/users/view.php?id=1'); ?>" class="btn btn-success"><i class="far fa-eye"></i> View</a>
                 </div>
