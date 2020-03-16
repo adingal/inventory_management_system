c5929ba9c736dbb12cca86f9b89ab9a654ee5177
Alex Dingal <lhexie2014@gmail.com>
2020-03-17 01:52:46 +0800

    Create a function to find user by id

--- private/queries.php
+++ private/queries.php
@@ -102,6 +102,21 @@
         $result = mysqli_query($db, $sql); 
 
         return $result;
-    }    
+    }
+    
+    function find_user_by_id($id) {
+        global $db;
+
+        $sql = "SELECT * FROM users ";
+        $sql .= "WHERE user_id = '" . db_escape($db, $id) . "' ";
+        $sql .= "LIMIT 1";
+
+        $result = mysqli_query($db, $sql);
+        $user = mysqli_fetch_assoc($result);
+        mysqli_free_result($result);
+
+        // Returns associative array
+        return $user;
+    }
 
 ?>
