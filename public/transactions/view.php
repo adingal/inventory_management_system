46ca77e7718bbcd6cbbd6f65ced1e11dcb340ada
Alex Dingal <lhexie2014@gmail.com>
2020-03-24 01:03:08 +0800

    Create view transactions main content

--- public/transactions/view.php
+++ public/transactions/view.php
@@ -15,7 +15,40 @@
 <?php include(SHARED_PATH . '/main_header.php'); ?>
 
 <div id="content">
-
+    <div id="view_transaction" class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 mx-auto">
+                    <h1 class="display-5"><?php echo 'Transaction id: ' . h($transaction['transaction_id']); ?></h1>
+                    <div class="form-group">
+                        <label for="user_name">User Name</label>
+                        <input type="text" class="form-control" name="user_name" value="<?php echo h($transaction['user_id']); ?>" disabled>
+                    </div>
+                    <div class="form-group">
+                        <label for="item_name">Item Name</label>
+                        <input type="text" class="form-control" name="item_name" value="<?php echo h($transaction['item_id']); ?>" disabled>
+                    </div>
+                    <div class="form-group">
+                        <label for="quantity">Quantity</label>
+                        <input type="number" class="form-control" name="quantity" value="<?php echo h($transaction['withdrawn_quantity']); ?>" disabled>
+                    </div>
+                    <div class="form-group">
+                        <label for="transaction_date">Transaction Date</label>
+                        <input type="text" class="form-control" name="transaction_date" value="<?php echo h($transaction['transaction_date']); ?>" disabled>
+                    </div>
+                    <div class="form-group">
+                        <label for="remarks">Remarks</label>
+                        <textarea class="form-control" name="remarks" cols="30" rows="5" disabled><?php echo h($transaction['remarks']); ?></textarea>
+                    </div>
+                    <div class="form-group text-right">
+                        <a href="<?php echo url_for('/transactions/edit.php?id=' . h(u($id))); ?>" class="btn btn-dark">Edit</a>
+                        <a href="<?php echo url_for('/transactions/delete.php?id=' . h(u($id))); ?>" class="btn btn-dark">Delete</a>
+                        <a href="<?php echo url_for('/transactions/index.php'); ?>" class="btn btn-dark">Cancel</a>
+                    </div>                    
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php include(SHARED_PATH . '/main_footer.php'); ?>
