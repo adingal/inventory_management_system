3b481dc6ab50261d3e9d89b6012f28817d44f375
Alex Dingal <lhexie2014@gmail.com>
2020-03-26 19:07:02 +0800

    Modify public index page, remove recent history

--- public/index.php
+++ public/index.php
@@ -31,87 +31,34 @@
         <?php } else { ?>
             <div id="home" class="mt-5">
                 <div class="container">
+                    <h1 class="display-4 mb-5 pb-2">Tracking</h1>
                     <div class="row">
-                        <div class="col-md-9">
-                            <h1 class="mb-3">Recent Transactions</h1>
-                            <table class="table">
-                                <thead class="thead-dark">
-                                    <tr>
-                                        <th>Item Name</th>
-                                        <th>Description</th>
-                                        <th>Quantity</th>
-                                        <th>Withdrawn By</th>
-                                        <th>Withdraw Date</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>Item 1</td>
-                                        <td>Item 1 description</td>
-                                        <td>5</td>
-                                        <td>Jose Manalo</td>
-                                        <td>March 5, 2020</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Item 2</td>
-                                        <td>Item 2 description</td>
-                                        <td>3</td>
-                                        <td>Jose Manalo</td>
-                                        <td>March 4, 2020</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Item 3</td>
-                                        <td>Item 3 description</td>
-                                        <td>2</td>
-                                        <td>Jose Manalo</td>
-                                        <td>March 7, 2020</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Item 1</td>
-                                        <td>Item 1 description</td>
-                                        <td>5</td>
-                                        <td>Jose Manalo</td>
-                                        <td>March 5, 2020</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Item 2</td>
-                                        <td>Item 2 description</td>
-                                        <td>3</td>
-                                        <td>Jose Manalo</td>
-                                        <td>March 4, 2020</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Item 3</td>
-                                        <td>Item 3 description</td>
-                                        <td>2</td>
-                                        <td>Jose Manalo</td>
-                                        <td>March 7, 2020</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        <div class="col-md-3">
+                        <div class="col-md-4">
                             <div class="card bg-primary text-center text-white mb-3">
                                 <div class="card-body">
-                                    <h2>Items</h2>
+                                    <h2 class="display-5">Items</h2>
                                     <h3 class="display-4">
                                         <i class="fas fa-boxes"></i>
                                     </h3>
                                     <a href="" class="btn btn-outline-light btn-sm">View</a>
                                 </div>
                             </div>
+                        </div>
+                        <div class="col-md-4">
                             <div class="card bg-success text-center text-white mb-3">
                                 <div class="card-body">
-                                    <h2>Transactions</h2>
+                                    <h2 class="display-5">Transactions</h2>
                                     <h3 class="display-4">
                                         <i class="far fa-edit"></i>
                                     </h3>
                                     <a href="" class="btn btn-outline-light btn-sm">View</a>
                                 </div>
                             </div>
+                        </div>
+                        <div class="col-md-4">
                             <div class="card bg-danger text-center text-white mb-3">
                                 <div class="card-body">
-                                    <h2>Users</h2>
+                                    <h2 class="display-5">Users</h2>
                                     <h3 class="display-4">
                                         <i class="fa fa-user"></i>
                                     </h3>
