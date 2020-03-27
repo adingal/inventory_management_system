ac36ec18864748d6445012f3e44557cb2ca2dd8d
Alex Dingal <lhexie2014@gmail.com>
2020-03-27 22:50:28 +0800

    Create two functions for testing character length

--- private/validations.php
+++ private/validations.php
@@ -6,4 +6,18 @@
         return !isset($value) || trim($value) === '';
     }
 
+    // Test whether given value pass the minimum character length
+    // Returns boolean
+    function has_length_greater($value, $min) {
+        $length = strlen($value);
+        return $length > $min;
+    }
+
+    // Test whether given value does not exceed the maximum character length
+    // Returns boolean    
+    function has_length_less($value, $max) {
+        $length = strlen($value);
+        return $length < $max;
+    }
+
 ?>
