180b49d077256d47c5b2780eeb04a99f2f922612
Alex Dingal <lhexie2014@gmail.com>
2020-03-30 00:52:11 +0800

    Set withdraw item page contents

--- public/items/withdraw.php
+++ public/items/withdraw.php
@@ -9,6 +9,8 @@
         redirect_to(url_for('/items/index.php'));
     }
 
+    $item = find_item_by_id($id);
+
 ?>
 
 <?php include(SHARED_PATH . '/main_header.php'); ?>
@@ -21,15 +23,16 @@
                     <form action="<?php echo url_for('items/withdraw.php?id=' . h(u($id))); ?>" method="post">
                         <div class="form-group">
                             <label for="item_id">Item ID</label>
-                            <input type="text" class="form-control mb-2" name="item_id">
+                            <input type="text" class="form-control mb-2" name="item_id" value="<?php echo h($id); ?>" disabled>
                         </div>
                         <div class="form-group">
                             <label for="item_name">Item Name</label>
-                            <input type="text" class="form-control" name="item_name">
+                            <input type="text" class="form-control" name="item_name" value="<?php echo h($item['item_name']); ?>" disabled>
                         </div>
                         <div class="form-group">
                             <label for="quantity">Quantity</label>
-                            <input type="number" class="form-control" name="quantity" min="1">
+                            <input type="number" class="form-control" name="quantity" min="1" max="<?php echo h($item['quantity']); ?>" value="1">
+                            <small class="text-danger">Available quantity: <?php echo h($item['quantity']); ?></small>
                         </div>
                         <div class="form-group text-right">
                             <button class="btn btn-dark" data-dismiss="modal">Withdraw</button>
