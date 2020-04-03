7f38c41e05e6a357536ea549254933246af3ad58
Alex Dingal <lhexie2014@gmail.com>
2020-04-04 01:10:04 +0800

    Reset users table

--- public/users/users.sql
+++ public/users/users.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     user_id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(255) NOT NULL,
@@ -8,42 +9,3 @@ CREATE TABLE users (
     PRIMARY KEY (user_id),
     INDEX (registered_date)
 ) ENGINE = INNODB;
-
--- Dummy data
-INSERT INTO users (first_name, last_name, email, hashed_password, registered_date)
-VALUES 
-    (
-        'Jose',
-        'Manalo',
-        'jm@gmail.com',
-        'jm123',
-        NOW()
-    ),
-    (
-        'Juan',
-        'Dela Cruz',
-        'jdc@aol.com',
-        'jdc123',
-        NOW()
-    ),
-    (
-        'Pedro',
-        'Agbayani',
-        'pa@yahoo.com',
-        'pa123',
-        NOW()
-    ),
-    (
-        'John',
-        'Escoto',
-        'je@gmail.com',
-        'je123',
-        NOW()
-    ),
-    (
-        'Ricky',
-        'Jones',
-        'rj@gmail.com',
-        'rj123',
-        NOW()
-    );
