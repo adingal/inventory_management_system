dab5d96cdb89cfd8c3d6413d707fc497b3a22633
Alex Dingal <lhexie2014@gmail.com>
2020-04-09 12:25:40 +0800

    Add user type on database schema

--- public/users/users.sql
+++ public/users/users.sql
@@ -6,6 +6,8 @@ CREATE TABLE users (
     email VARCHAR(255) NOT NULL,
     hashed_password VARCHAR(255) NOT NULL,
     registered_date DATETIME NOT NULL,
+    -- Add user type to handle admin and normal users
+    user_type CHAR(5),
     PRIMARY KEY (user_id),
     INDEX (registered_date)
 ) ENGINE = INNODB;
