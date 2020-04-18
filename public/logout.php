931f2313427045a07dc3a5a81bac6ef432a579d4
Alex Dingal <lhexie2014@gmail.com>
2020-04-18 13:00:30 +0800

    Add comments to logout page

--- public/logout.php
+++ public/logout.php
@@ -1,7 +1,14 @@
 <?php
+    /*
+    * Logout Page  
+    */
+
+    // Rquire init file
     require_once('../private/init.php');
 
+    // Call log out user function
     log_out_user();
 
+    // Redirect to login page
     redirect_to(url_for('/login.php'));
 ?>
