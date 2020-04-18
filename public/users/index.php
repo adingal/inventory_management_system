89d3bc869aa500984c689b3ae0c87dd9d772d4f4
Alex Dingal <lhexie2014@gmail.com>
2020-04-18 12:47:51 +0800

    Add comments to user index page

--- public/users/index.php
+++ public/users/index.php
@@ -1,15 +1,25 @@
 <?php
+    /*
+    * User Index Page  
+    */
+
+    // Require init file
     require_once('../../private/init.php');
 
+    // Require login
     require_login();
 
+    // Set page title
     $page_title = 'Users';
 
-    $users = find_all_users();    
+    // Call find all users function
+    $users = find_all_users();
+
+    // Call find all users id function
     $user_ids = find_all_users_id();
 ?>
 
-<?php include(SHARED_PATH . '/main_header.php'); ?>
+<?php include(SHARED_PATH . '/main_header.php'); // Include header file ?>
 
 <div id="content">
     <div id="actions" class="pt-5 pb-2 mt-3">
@@ -21,22 +31,28 @@
                             <label class="input-group-text" for="selected_id">User ID</label>
                         </div>
                         <select class="custom-select" id="selected_id">
+
+                            <!-- Loop on all user id and create options -->
                             <?php
                                 while ($user_id = mysqli_fetch_row($user_ids)) {
                                     echo '<option value="' . h($user_id[0]) . '">';
                                     echo h($user_id[0]) . '</option>';
                                 }
-                            ?>      
+
+                                // Free up resources
+                                mysqli_free_result($user_ids);
+                            ?>
+
                         </select>
                     </div>
-                </div>
+                </div> <!-- id_options -->
                 <div class="col-md-8 col-lg-9 text-center text-md-right">
                     <a href="<?php echo url_for('/users/add.php'); ?>" class="btn btn-primary"><i class="fa fa-plus"></i> Add</a>
                     <a href="<?php echo url_for('/users/view.php?id=1'); ?>" class="btn btn-success"><i class="far fa-eye"></i> View</a>
-                </div>
-            </div>
-        </div>
-    </div>
+                </div> <!-- col-md-8 -->
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </div> <!-- actions -->
     <div id="users">
         <div class="container">
             <div class="row">
@@ -53,7 +69,10 @@
                             </tr>
                         </thead>
                         <tbody>
+
+                            <!-- Loop on all users and create row -->
                             <?php while ($user = mysqli_fetch_assoc($users)) { ?>
+
                                 <tr>
                                     <td><?php echo h($user['user_id']); ?></td>
                                     <td><?php echo h($user['first_name']); ?></td>
@@ -62,14 +81,20 @@
                                     <td><?php echo h($user['registered_date']); ?></td>
                                     <td><?php echo h($user['user_type']); ?></td>
                                 </tr>
-                            <?php } ?>
+                                
+                            <?php
+                                }
+
+                                // Free up resources
+                                mysqli_free_result($users);
+                            ?>
+
                         </tbody>
                     </table>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+                </div> <!-- col -->
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </div> <!-- users -->
+</div> <!-- content -->
 
-<?php mysqli_free_result($users); ?>
-<?php include(SHARED_PATH . '/main_footer.php'); ?>
+<?php include(SHARED_PATH . '/main_footer.php'); // Include footer file ?>
